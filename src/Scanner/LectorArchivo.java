79338d81ba4973bcbc31107c9d0df88b9f01c5cf
Ivan Sanchez <ivanhumberto_16@hotmail.com>
2020-09-22 15:03:24 -0600

    Primer commit

---
+++ src/Scanner/LectorArchivo.java
@@ -0,0 +1,31 @@
+package Scanner;
+
+import java.io.*;
+
+public class LectorArchivo {
+    private File arch;
+    private FileReader fr;
+    private BufferedReader br;
+
+    public LectorArchivo(String nom){
+        try {
+            arch=new File("src/"+nom+".txt");
+            fr = new FileReader(arch);
+            br = new BufferedReader(fr);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void reiniciarApuntador() throws IOException {
+        fr = new FileReader(arch);
+        br = new BufferedReader(fr);
+    }
+
+    public String leerSigLinea() throws IOException {
+        String linea=br.readLine();
+        if(linea==null)
+            return null;
+        return linea;
+    }
+}
